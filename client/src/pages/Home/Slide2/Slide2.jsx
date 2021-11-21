93ec2116eca306f862b0752aaa7e87e5e519aa6e
Raj Aryan <silvergraph@pop-os.localdomain>
2021-11-21 20:10:02 +0530

    Setup directory for client and server

---
+++ client/src/pages/Home/Slide2/Slide2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Slide2.css"
+
+export default function Slide2() {
+    return (
+        <div className="container-slide-2">
+            Slide2
+        </div>
+    )
+}
