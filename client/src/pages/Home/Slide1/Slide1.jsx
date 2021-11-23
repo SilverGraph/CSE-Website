8450e07c375be826644f5b472b77563b80dfa3ef
SilverGraph <rj.rajeryan0071@gmail.com>
2021-11-23 22:46:42 +0530

    Added scrolling effect on homepage

--- client/src/pages/Home/Slide1/Slide1.jsx
+++ client/src/pages/Home/Slide1/Slide1.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import "./Slide1.css"
 
 export default function Slide1() {
+
     return (
-        <div className="container-slide-1">
-            Slide1
+        <div data-aos="fade-up" className="container-slide-1">
+            <h1>SLIDE 1</h1>
         </div>
     )
 }
