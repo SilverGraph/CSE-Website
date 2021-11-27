6c875646cf588a3bf8ec1af6eb30b8169c07ca66
Piyush Mishra <piyushmishra965@gmail.com>
2021-11-27 19:32:58 +0530

    server running

---
+++ server/database.py
@@ -0,0 +1,24 @@
+import pymongo
+
+class Database:
+    myclient = pymongo.MongoClient('mongodb+srv://RajAryan:Sgjpn0Tfz0uu1lMj@cluster0.aug5r.mongodb.net/test?retryWrites=true&w=majority')
+    db = myclient['test']
+    col = db['newTest']
+
+    @classmethod
+    def insert(cls, document):
+        cls.col.insert_one(document)
+
+    @classmethod
+    def delete(cls, email):
+        cls.col.delete_one({'email': email})
+
+    @classmethod
+    def update(cls, email, new_pwd_hash = '', new_email = ''):
+        if new_email == '':
+            new_email = email
+        cls.col.update_one({'email': email}, {"$set": {'email': new_email, 'pwd_hash': new_pwd_hash}})
+
+
+if __name__=='__main__':
+    Database.delete('DaddyCool')
